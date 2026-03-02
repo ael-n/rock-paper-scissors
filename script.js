cbaacc5507aac83f438c7067d90661a7e09bc2f5
ael-n <edwidgearie@gmail.com>
2026-03-02 10:57:57 -0800

    create get cumputer fonction, human choice and  paly score

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("Hello World")
+function getComputerChoice () {
+    const randomNumber = Math.random()
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
+function getHumanChoice () {
+    const choice = prompt("Enter Rock, Paper or Scissors!")
+    return choice;
+}
+
+function playerScore () {
+    let humanScore = 0;
+    let computerScore = 0;
+}
+
+
         
